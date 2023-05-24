deca1fdd57aa92b2a1948b2ff78244ac58a903aa
Noah4ever <noah.thiering@web.de>
2023-05-24 19:02:14 +0200

    feat: add comments and remove not working echo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-# If directory exists
+# If directory exists, exit
 if [ -d "$HOME/.tmux/plugins/tpm" ]; then
     exit
 fi
 
-echo "Cloning tmux plugin manager ($HOME/.tmux/plugins/tpm)"
-
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Source the tmux configuration file
 tmux source $HOME/.config/tmux/tmux.conf
 
+# Install plugins using TPM
 $HOME/.tmux/plugins/tpm/scripts/install_plugins.sh
